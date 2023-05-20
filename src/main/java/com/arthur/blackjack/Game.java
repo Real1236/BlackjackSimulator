490ef5c2ea862877babbbfba5898601f713aacca
Real1236 <arthurchan2001@live.com>
2023-05-20 12:25:33 -0400

    Moved methods to Player class to be more SOLID

--- src/main/java/com/arthur/blackjack/Game.java
+++ src/main/java/com/arthur/blackjack/Game.java
@@ -1,12 +1,12 @@
 package com.arthur.blackjack;
 
-import com.arthur.blackjack.component.Card;
 import com.arthur.blackjack.component.Deck;
-import com.arthur.blackjack.component.Hand;
 import com.arthur.blackjack.player.Dealer;
 import com.arthur.blackjack.player.Player;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Game {
     private Deck deck;
@@ -42,14 +42,13 @@ public class Game {
         int numOfPlayers = 1;
         int startingBankroll = 1000;
 
-        for (int i = 0; i < numOfPlayers; i++) {
-            players.add(new Player(i + 1, startingBankroll));
-        }
+        for (int i = 1; i <= numOfPlayers; i++)
+            players.add(new Player(i, startingBankroll));
 
         // Loop game until all players are bankrupt (they'll be deleted)
-        while (players.size() > 0) {
+        while (players.size() > 0)
             startRound();
-        }
+
         System.out.println("GG");
     }
 
@@ -65,7 +64,7 @@ public class Game {
 
         // Players take their turns
         for (Player player : players) {
-            takeTurn(player);
+            player.takeTurn(minimumBet, dealer, deck);
         }
 
         // Dealer takes its turn
@@ -73,84 +72,6 @@ public class Game {
 
         // Determine the winners and pay out
         determineWinners();
-    }
-
-    private void takeTurn(Player player) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("\nPlayer " + player.getId() + "'s turn");
-        System.out.println("Money: " + player.getMoney());
-        System.out.println("\nPlace your bet:");
-        int bet = Integer.parseInt(scanner.nextLine());
-        while (bet < minimumBet) {
-            System.out.println("Place a bet greater than or equal to the minimum bet:");
-            bet = Integer.parseInt(scanner.nextLine());
-        }
-        player.getHand().setBet(bet);
-        playHand(player, 0);
-    }
-
-    private void playHand(Player player, int hand) {
-        player.placeBet(hand);
-        boolean hasStood = false;
-
-        while (player.getHand(hand).getTotal() < 21 && !hasStood) {
-            System.out.println("\nYour cards: " + player.getHand(hand));
-            System.out.println("Your score: " + player.getHand(hand).getTotal());
-            System.out.println("Dealer's upcard: " + dealer.getHand().getCards().get(0));
-
-            String choice = getPlayerChoice(player, hand);
-            hasStood = performPlayerAction(player, choice, hand);
-        }
-    }
-
-    private String getPlayerChoice(Player player, int hand) {
-        Scanner scanner = new Scanner(System.in);
-        Map<String, String> choices = getChoices(player, hand);
-
-        System.out.println("Choose an option: " + choices + "");
-        String choice = scanner.nextLine();
-
-        while (!choices.containsKey(choice)) {
-            System.out.println("Invalid choice, pick again:");
-            choice = scanner.nextLine();
-        }
-
-        return choice;
-    }
-
-    private Map<String, String> getChoices(Player player, int hand) {
-        Map<String, String> choices = new HashMap<>();
-        choices.put("h", "hit");
-        choices.put("s", "stand");
-        if (player.canDouble(hand))
-            choices.put("d", "double down");
-        if (player.canSplit(hand))
-            choices.put("p", "split");
-        return choices;
-    }
-
-    private boolean performPlayerAction(Player player, String choice, int hand) {
-        switch (choice) {
-            case "h" -> dealer.dealCard(player, hand, deck);
-            case "s" -> { return true; }
-            case "d" -> {
-                player.placeBet(hand);
-                int curBet = player.getHand(hand).getBet();
-                player.getHand(hand).setBet(curBet * 2);
-                dealer.dealCard(player, hand, deck);
-                return true;
-            }
-            case "p" -> {
-                Card temp = player.getHand(hand).removeCard();
-                dealer.dealCard(player, hand, deck);
-                player.addHand(player.getHand(hand).getBet());
-                player.getHand().addCard(temp);
-                dealer.dealCard(player, deck);
-                playHand(player, player.getNumOfHands() - 1);
-            }
-            default -> System.out.println("Invalid choice. Please choose 'h', 's', or 'p'.");
-        }
-        return false;
     }
 
     private void determineWinners() {
@@ -170,7 +91,8 @@ public class Game {
         int numOfHands = player.getNumOfHands();
 
         for (int i = 1; i <= numOfHands; i++) {
-            evaluateHand(player, i);
+            player.evaluateHand(i, dealer);
+            System.out.println();
             player.clearHand(0);
         }
 
@@ -179,29 +101,6 @@ public class Game {
             System.out.println("Player " + player.getId() + " is eliminated from the game!\n");
             playersToRemove.add(player);
         }
-    }
-
-    private void evaluateHand(Player player, int handIndex) {
-        Hand hand = player.getHand(0);
-        System.out.println("Player " + player.getId() + " - Hand " + handIndex + ": " + hand);
-        System.out.println("Player " + player.getId() + " - Hand " + handIndex + " score: " + hand.getTotal());
-
-        if (hand.getTotal() == 21 && hand.getCards().size() == 2) {
-            System.out.println("Player " + player.getId() + " - Hand " + handIndex + " gets Blackjack!");
-            player.winBlackjack(0);
-        } else if (hand.getTotal() > 21) {
-            System.out.println("Player " + player.getId() + " - Hand " + handIndex + " busts!");
-        } else if (dealer.getHand().getTotal() > 21 || hand.getTotal() > dealer.getHand().getTotal()) {
-            System.out.println("Player " + player.getId() + " - Hand " + handIndex + " wins!");
-            player.winBet(0);
-        } else if (hand.getTotal() == dealer.getHand().getTotal()) {
-            System.out.println("Player " + player.getId() + " - Hand " + handIndex + " pushes.");
-            player.pushBet(0);
-        } else {
-            System.out.println("Player " + player.getId() + " - Hand " + handIndex + " loses.");
-        }
-
-        System.out.println();
     }
 
 }

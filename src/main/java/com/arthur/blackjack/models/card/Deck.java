5c81fd3de103179f5ac7ddff9a82d80a73a8ebae
Real1236 <arthurchan2001@live.com>
2024-01-03 12:20:13 -0500

    fix: Remove unused imports in Deck and HandFactory

--- src/main/java/com/arthur/blackjack/models/card/Deck.java
+++ src/main/java/com/arthur/blackjack/models/card/Deck.java
@@ -1,7 +1,5 @@
 package com.arthur.blackjack.models.card;
 
-import com.arthur.blackjack.models.card.Card;
-
 public interface Deck {
     void reshuffleDeck();
     Card dealCard();
--- src/main/java/com/arthur/blackjack/models/hand/HandFactory.java
+++ src/main/java/com/arthur/blackjack/models/hand/HandFactory.java
@@ -1,8 +1,5 @@
 package com.arthur.blackjack.models.hand;
 
-import com.arthur.blackjack.models.hand.DealerHand;
-import com.arthur.blackjack.models.hand.PlayerHand;
-
 public interface HandFactory {
     PlayerHand createPlayerHand();
     DealerHand createDealerHand();

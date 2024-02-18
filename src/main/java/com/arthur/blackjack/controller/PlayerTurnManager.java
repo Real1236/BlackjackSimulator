0a052263b2bccc1637bbf54c3459b6ced97cc0a6
Real1236 <arthurchan2001@live.com>
2024-02-18 12:51:37 -0500

    Refactor Strategy bean implementation

--- src/main/java/com/arthur/blackjack/controller/PlayerTurnManager.java
+++ src/main/java/com/arthur/blackjack/controller/PlayerTurnManager.java
@@ -3,7 +3,6 @@ package com.arthur.blackjack.controller;
 import com.arthur.blackjack.models.card.Deck;
 import com.arthur.blackjack.models.player.Dealer;
 import com.arthur.blackjack.models.player.Player;
-import com.arthur.blackjack.strategies.Strategy;
 
 public interface PlayerTurnManager {
     void playerTurn(Dealer dealer, Player player, Deck deck);

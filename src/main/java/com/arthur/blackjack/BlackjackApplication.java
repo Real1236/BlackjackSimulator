61e25e51975876b8f5e84545c4e49e543692f2ef
Real1236 <arthurchan2001@live.com>
2024-08-12 20:07:07 -0400

    set up sim for old laptop

--- src/main/java/com/arthur/blackjack/BlackjackApplication.java
+++ src/main/java/com/arthur/blackjack/BlackjackApplication.java
@@ -40,13 +40,13 @@ public class BlackjackApplication implements CommandLineRunner {
 		this.gameSettings.setMinChipSize(1F);
 		this.gameSettings.setBetSpread(12);
 		this.gameSettings.setBankroll(10000 * this.gameSettings.getBetSize()); // 10000 betting units = <1% risk of ruin
-		this.gameSettings.setMaxRounds(1000000);
+		this.gameSettings.setMaxRounds(100000);
 
 		// Configure game rules
-		this.gameRules.setRule(Rule.FANDUEL_AMERICAN);
+		this.gameRules.setRule(Rule.NORMAL);
 
 		// Configure number of games to run
-		int numGames = 10;
+		int numGames = 3;
 		for (int i = 1; i <= numGames; i++) {
 			Game game = gameFactory.createGame(i);
 			game.start();
